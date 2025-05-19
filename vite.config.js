8fcf9249c1070eb99d9beacb1e36acffd8abf025
Vrushab Bayas <31801565+VrushabBayas@users.noreply.github.com>
2025-05-19 12:56:11 +0530

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mdx from "@mdx-js/rollup";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server :{
+  plugins: [react(), mdx()],
+  server: {
     port: 3000,
     open: true,
-  }
-})
+  },
+});
